1e027e038649024efcc4dee18fc86d3e367b36c4
John Mc.Queide Clemente <mcqueide@gmail.com>
2019-06-23 12:32:43 -0300

    Adicionandno PessoaRepositoryTest

---
+++ pessoa/src/test/java/br/com/arqdev/pessoa/repository/PessoaRepositoryTest.java
@@ -0,0 +1,81 @@
+package br.com.arqdev.pessoa.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.arqdev.pessoa.TestConfiguration;
+import br.com.arqdev.pessoa.entity.Pessoa;
+import br.com.arqdev.pessoa.entity.Telefone;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=TestConfiguration.class)
+public class PessoaRepositoryTest {
+
+	@Autowired
+	private PessoaRepository repository;
+	
+	@Before
+	public void setUp() {
+		Telefone telefone = Telefone.builder()
+				.numero("61934567654")
+				.build();
+		
+		Pessoa pessoa = Pessoa.builder()
+				.nome("fulano")
+				.dataNascimento(LocalDate.of(1992, 9, 19))
+				.telefones(Collections.singletonList(telefone))
+				.build();
+		
+		telefone.setPessoa(pessoa);
+		
+		repository.save(pessoa);
+	}
+	
+	@After
+	public void tearDown() {
+		repository.deleteAll();
+	}
+	
+	@Test
+	public void deveAcharPessoaFulano() {
+		Optional<Pessoa> pessoaOptional = repository.findById(1);
+		
+		boolean isPresent = pessoaOptional.isPresent();
+		
+		assertTrue(isPresent);
+		pessoaOptional.ifPresent(p -> {
+			assertEquals("fulano", p.getNome());
+			assertEquals(LocalDate.of(1992, 9, 19), p.getDataNascimento());
+			assertEquals(1, p.getTelefones().size());
+			assertEquals("61934567654", p.getTelefones().get(0).getNumero());
+		});
+	}
+	
+	@Test
+	public void deveAcharTodasPessoas() {
+		List<Pessoa> pessoas = repository.findAll();
+		
+		assertEquals(1, pessoas.size());
+		
+		Pessoa p = pessoas.get(0);
+		
+		assertEquals("fulano", p.getNome());
+		assertEquals(LocalDate.of(1992, 9, 19), p.getDataNascimento());
+		assertEquals(1, p.getTelefones().size());
+		assertEquals("61934567654", p.getTelefones().get(0).getNumero());
+	}
+	
+}
