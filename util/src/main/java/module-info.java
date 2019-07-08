c3731c2fcfa24e3247a38ed8c2d2d9dc9c5541c8
CesarDraw <cesardraw@gmail.com>
2019-07-08 19:48:02 -0300

    [FIX] Ajustes na exportação do módulo Util.

--- util/src/main/java/module-info.java
+++ util/src/main/java/module-info.java
@@ -3,20 +3,26 @@ module br.com.arqdev.util {
     requires spring.context;
     requires dozer;
     requires springfox.core;
-    //requires springfox.spi;
     requires springfox.spring.web;
     requires springfox.swagger2;
-    //requires google.collections;
     requires spring.web;
     requires spring.plugin.metadata;
     requires com.fasterxml.classmate;
-    //requires springfox.schema;
     requires spring.plugin.core;
-    //requires springfox.spi;
     requires spring.webmvc;
     requires spring.core;
     requires com.google.common;
-    //requires springfox.spi;
 
     exports br.com.arqdev.util;
+    exports br.com.arqdev.util.swagger.model;
+    exports br.com.arqdev.util.swagger.api;
+    exports br.com.arqdev.util.swagger.documentation;
+    exports br.com.arqdev.util.swagger.operation;
+    exports br.com.arqdev.util.swagger.parameter;
+    exports br.com.arqdev.util.swagger.request;
+    exports br.com.arqdev.util.swagger.resource;
+    exports br.com.arqdev.util.swagger.response;
+    exports br.com.arqdev.util.swagger.security;
+    exports br.com.arqdev.util.swagger.tag;
+    exports br.com.arqdev.util.swagger.type;
 }
